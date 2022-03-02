54fac6fc79f9f7d8ebc0de0523c07f2f18e3415f
Tribhuwan <mathincosmos@gmail.com>
2022-03-02 20:58:00 +0530

    var in bubblesort

--- Practice_Problems/Array/sorting tech/Bubble.cpp
+++ Practice_Problems/Array/sorting tech/Bubble.cpp
@@ -2,10 +2,18 @@
 using namespace std;
 
 void bubbleSort(int arr[] , int n){
+    int swap_counter = 1;
+    
     for (int i = 0; i < n; i++){
+        if(swap_counter==0)
+            break;
+        swap_counter = 0;
         for (int j = 0; j < n - i-1; j++){
-            if(arr[j]>arr[j+1])
+            if(arr[j]>arr[j+1]){
                 swap(arr[j], arr[j + 1]);
+                swap_counter++;
+        }
+        
         }
     }
 }
